29d0b6e19f7ae6e4eb30533b941a82a979832a01
Rafay <rafay.mail2004@gamil.com>
2023-10-12 10:13:12 +0600

    task11

--- lab-2/task11.c
+++ lab-2/task11.c
@@ -2,5 +2,19 @@
 
 int main()
 {
-    
+    int s;  //float L ta eikhane nile 2nd condition er jonno vul answer asbe
+
+    printf("Enter the value of S to calculate the value of L: ");
+    scanf("%d", &s);
+
+    if(s<100)
+    {
+        float l = 3000-125*(s*s);
+        printf("The value of L: %f", l);
+    }
+    else if(s>=100)
+    {
+        float l = 12000/(4+(s*s)/14900);
+        printf("The value of L: %f", l);
+    }
 }
