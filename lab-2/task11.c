f80a2154186c70aac8e34babd8c8c3f62001972b
Rafay <rafay.mail2004@gamil.com>
2023-10-12 14:07:03 +0600

    task11

--- lab-2/task11.c
+++ lab-2/task11.c
@@ -2,19 +2,21 @@
 
 int main()
 {
-    int s;  //float L ta eikhane nile 2nd condition er jonno vul answer asbe
+    int s;  
 
     printf("Enter the value of S to calculate the value of L: ");
     scanf("%d", &s);
 
     if(s<100)
     {
-        float l = 3000-125*(s*s);
+        float l = 3000-125*((float)s*s); //typecast
         printf("The value of L: %f", l);
     }
     else if(s>=100)
     {
-        float l = 12000/(4+(s*s)/14900);
+        float l = 12000/(4+((float)s*s)/14900);
         printf("The value of L: %f", l);
     }
+
+    return 0;
 }
