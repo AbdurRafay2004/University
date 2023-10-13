1b0dce62d384b425447280cb71ef50a8ccd1cf55
Abdur Rafay <rafay.mail2004@gmail.com>
2023-10-13 21:49:31 +0600

    task13

--- lab-2/task11.c
+++ lab-2/task11.c
@@ -8,15 +8,15 @@ int main()
     scanf("%d", &s);
 
     if(s<100)
-    {
-        float l = 3000-125*((float)s*s); //typecast
-        printf("The value of L: %f", l);
-    }
+        {
+            float l = 3000-125*((float)s*s); //typecast
+            printf("The value of L: %f", l);
+        }
     else if(s>=100)
-    {
-        float l = 12000/(4+((float)s*s)/14900);
-        printf("The value of L: %f", l);
-    }
+        {
+            float l = 12000/(4+((float)s*s)/14900);
+            printf("The value of L: %f", l);
+        }
 
     return 0;
 }
--- lab-2/task13.c
+++ lab-2/task13.c
@@ -2,5 +2,39 @@
 
 int main()
 {
-    
+    int point;
+    printf("Enter your marks: ");
+    scanf("%d", &point);
+
+    if(point>=0 && point<=100)
+        {
+            if(point>=90 && point<=100)
+                {
+                    printf("A");
+                }
+            else if(point>=80 && point<=89)
+                {
+                    printf("B");
+                }
+            else if(point>=70 && point<=79)
+                {
+                    printf("C");
+                }
+            else if(point>=60 && point<=69)
+                {
+                    printf("D");
+                }
+            else if(point>=50 && point<=59)
+                {
+                    printf("E");
+                }
+            else if(point<50)
+                {
+                    printf("F");
+                }
+        }
+    else
+        {
+            printf("Invalid number");
+        }
 }
