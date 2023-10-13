72c057d9a6176245a06d8058ffc13436bf7102a7
Abdur Rafay <rafay.mail2004@gmail.com>
2023-10-14 01:27:32 +0600

    task23

--- lab-2/task23.c
+++ lab-2/task23.c
@@ -2,5 +2,22 @@
 
 int main()
 {
-    
+    double fahrenheit;
+    printf("Input temperature in fahrenheit: ");
+    scanf("%lf", &fahrenheit);
+
+    double celsius = (fahrenheit-32)*0.56;
+    printf("%0.2lf degrees C\n", celsius);
+
+    if(celsius<20)
+        {
+            printf("Winter");
+        }
+    else if(celsius>=20 && celsius<=25)
+        printf("Autumn");
+    else if(celsius>25 && celsius<30)
+        printf("Spring");
+    else
+        printf("Summer");
+
 }
