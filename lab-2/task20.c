e0604add634905ac258bbe521ae90b6302035312
Abdur Rafay <rafay.mail2004@gmail.com>
2023-10-13 23:34:12 +0600

    task20

--- lab-2/task20.c
+++ lab-2/task20.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
-
 int main()
 {
+    int number;
+    printf("Enter a number: ");
+    scanf("%d", &number);
     
+    if(!(number % 2 == 0 || number % 5 == 0))
+    //if (number % 2 != 0 && number % 5 !=0 ) 
+    //if (!(number % 2 == 0) && !(number % 5 == 0))
+    {
+        printf("%d",number);
+    }
+    else
+        printf("No");
 }
