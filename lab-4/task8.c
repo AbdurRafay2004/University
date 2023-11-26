33a41d1ce6487ad22d6e3862a93c9e8a5fb07481
Tahsin2004 <rafay.mail2004@gmail.com>
2023-11-26 22:46:31 +0600

    task8

--- lab-4/task8.c
+++ lab-4/task8.c
@@ -1,5 +1,57 @@
 #include <stdio.h>
 int main()
 {
+    int arr1[] = {1, 2, 3, 4, 5, 6, 7, 8, 9}, arr2[] = {10, 11, 12, -13, -14, -15, -16};
+
+    int arr1_size = sizeof(arr1)/sizeof(arr1[0]), arr2_size= sizeof(arr2)/sizeof(arr2[0]);
+    // printf("%d, %d", arr1_size, arr2_size);
+    int element=0, i, new_arr[element];
+
+    printf("Array 1: [");
+    for(i=0; i<arr1_size; i++)
+        {
+            printf("%d",arr1[i]);
+            if(i!=arr1_size-1)
+                {
+                    printf(", ");
+                }
+            if(arr1[i]%2==0)
+                {
+                    new_arr[element] = arr1[i];
+                    element++;
+                }
+        }
+    printf("]\n");
+
+    printf("Array 1: [");
+    for(i= 0; i<arr2_size; i++)
+        {
+            printf("%d",arr2[i]);
+            if(i!=arr2_size-1)
+                {
+                    printf(", ");
+                }
+            if(arr2[i]%2==0)
+                {
+                    new_arr[element]= arr2[i];
+                    element++;
+                }
+        }
+    printf("]\n\n");
     
+    printf("Even elements: [");
+    for(i=0; i<element; i++)
+        {
+            printf("%d", new_arr[i]);
+            if(i!=element-1)
+                {
+                    printf(", ");
+                }
+        }
+    printf("]\n\n");
+
+
+        // printf("Number in the Array: [%d, %d, %d, %d, %d, %d, %d, %d, %d, %d]", new_arr[0], new_arr[1], new_arr[2], new_arr[3], new_arr[4], new_arr[5], new_arr[6], new_arr[7], new_arr[8], new_arr[9]);
+
+
 }
--- lab-4/test.c
+++ lab-4/test.c
@@ -1,23 +1,34 @@
 #include <stdio.h>
 
 int main() {
-    int arr1[] = {1, 4, 7, 5};
-    int arr2[] = {6, 1, 3, 9};
-    int n1 = sizeof(arr1) / sizeof(arr1[0]);
-    int n2 = sizeof(arr2) / sizeof(arr2[0]);
-    int new_arr[n1 + n2 - 1];
-    for (int i = 0; i < n1 + n2 - 1; i++) {
-        if (i < n1 - 1) {
-            new_arr[i] = arr1[i];
-        } else {
-            new_arr[i] = arr2[i - n1 + 1];
+    int array_one[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int array_two[] = {10, 11, 12, -13, -14, -15, -16};
+    int n1 = sizeof(array_one) / sizeof(array_one[0]);
+    int n2 = sizeof(array_two) / sizeof(array_two[0]);
+
+    int new_arr[n1 + n2];
+
+    int j = 0;
+    
+    printf("[");
+    for (int i = 0; i < n1; i++) {
+        if (array_one[i] % 2 == 0) {
+            new_arr[j] = array_one[i];
+            j++;
+            printf("%d", array_one[i]);
+            if (j != n1 + n2) {
+                printf(", ");
+            }
         }
     }
-    printf("[");
-    for (int i = 0; i < n1 + n2 - 1; i++) {
-        printf("%d", new_arr[i]);
-        if (i != n1 + n2 - 2) {
-            printf(", ");
+    for (int i = 0; i < n2; i++) {
+        if (array_two[i] % 2 == 0) {
+            new_arr[j] = array_two[i];
+            j++;
+            printf("%d", array_two[i]);
+            if (j != n1 + n2) {
+                printf(", ");
+            }
         }
     }
     printf("]\n");
