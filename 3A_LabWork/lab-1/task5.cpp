2f55eb6511a01fb96d7f868d281e4967a8b8bf77
Tahsin2004 <rafay.mail2004@gmail.com>
2024-01-26 01:18:38 +0600

    task 5 and 6

---
+++ 3A_LabWork/lab-1/task5.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    //question 1
+    cout<< "Question 1:"<< endl;
+    float isfloat = 10.5;
+    int isint = (int)isfloat; //Casting float to int
+
+    cout << "The float value: "<< isfloat << endl 
+    << "Casting to int: " << isint<< endl << endl;
+    
+    //question 2
+    cout<< "Question 2:"<< endl;
+    double isDouble = 15.5;
+    int isInt = (int)isDouble;
+
+    cout<<"The Double value: "<<isDouble<<endl
+    <<"Casting to int: "<< isInt <<endl;
+
+    return 0;
+}
---
+++ 3A_LabWork/lab-1/task6.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    //question 1
+    cout<<"Question 1:"<<endl;
+    cout<<"Remainder of 15/4 = "<< 15%4 <<endl;
+    cout<<endl;
+    //question 2
+    cout<<"Question 2:"<<endl;
+    int x = 5;
+    int y = 5;
+    // ++x;
+    y--;
+    cout<<"x++ = "<< x++ << endl <<"y-- = "<< y <<endl;
+    return 0;
+}
