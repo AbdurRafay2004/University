488fab3fe7393ad2ba82c7e3c45b08f322fec856
Tahsin2004 <rafay.mail2004@gmail.com>
2024-01-26 01:45:55 +0600

    task7

--- 3A_LabWork/lab-1/task7.cpp
+++ 3A_LabWork/lab-1/task7.cpp
@@ -4,8 +4,18 @@ using namespace std;
 int main(){
     //question 1
     cout<<"Question 1:"<<endl;
-    
+    string city, country, location;
+    city = "Dhaka";
+    country = "Bangladesh";
+    location = city + ", " + country;
+    cout<<"Location: "<< location <<endl;
+    cout<<endl;
+
     //question 2
     cout<<"Question 2:"<<endl;
+    string book_title, author;
+    book_title = "Kishor Alo";
+    author = "Prothom Alo";
+    cout<<book_title + ", " + author<<endl;
     return 0;
 }
