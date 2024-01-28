2f03bd47004ec4b193192d0c5501a4dcdd33aaca
Tahsin2004 <rafay.mail2004@gmail.com>
2024-01-28 23:39:03 +0600

    task12

---
+++ 3A_LabWork/lab-1/task12.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    //question 1
+    cout<<"Question 1:"<<endl;
+    int input;
+    while(true){
+        cout<< "Enter -1 to exit: ";
+        cin >> input;
+        if(input == -1) break;
+    }
+    cout<< "Your are free now!"<< endl;
+    cout<<endl;
+    //question 2
+    cout<<"Question 2:"<<endl;
+    
+    return 0;
+}
